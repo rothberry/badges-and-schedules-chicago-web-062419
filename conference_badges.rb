3815684ac5fb088856c55ac49289786cf88ea952
Phil Roth <phil.roth077@gmail.com>
2019-06-06 01:19:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,6 +17,10 @@ def assign_rooms(array)
   end
 end
 def printer(attendees)
-
-
+  batch_badge_creator(attendees).each do |value|
+    puts value
+  end
+  assign_rooms(attendees).each do |value|
+    puts value
+  end
 end
